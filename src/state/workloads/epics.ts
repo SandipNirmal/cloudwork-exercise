67c5418c487b63d0d7941a7cfcbc4d193d1b1075
Sandip Nirmal <nirmal.sandip13389@gmail.com>
2022-04-08 10:23:57 +0530

    fix: changes to fix all epics

--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -1,7 +1,15 @@
 import { combineEpics, Epic } from 'redux-observable';
-import { merge } from 'rxjs';
-import { filter, map, tap, ignoreElements, switchMap, catchError } from 'rxjs/operators';
+import { merge, from } from 'rxjs';
+import {
+  filter,
+  map,
+  tap,
+  // ignoreElements,
+  delay,
+  mergeMap
+} from 'rxjs/operators';
 import { isActionOf } from 'typesafe-actions';
+import 'rxjs'
 
 import { RootAction, RootState } from '../reducer';
 import * as workloadsActions from './actions';
@@ -17,25 +25,82 @@ const logWorkloadSubmissions: AppEpic = (action$, state$) => (
     filter(isActionOf(workloadsActions.submit)),
     map(action => action.payload),
     tap((payload) => console.log('Workload submitted', payload)),
-    switchMap(payload => workloadService.create(payload)),
+    mergeMap(payload => workloadService.create(payload)),
     tap((payload) => console.log('Workload created', payload)),
-    map(payload => workloadsActions.created(payload))
+    map(payload => workloadsActions.created(payload)),
   )
-);
+)
 
 const cancelWorkload: AppEpic = (action$, state$) => (
   action$.pipe(
     filter(isActionOf(workloadsActions.cancel)),
     map(action => action.payload),
-    switchMap(payload => merge(workloadService.cancel(payload)).pipe(
-      map(payload => workloadsActions.updateStatus(payload)),
-      // catchError(err => Promise.resolve({ type: 'Error', message: err.message })
-    )))
+    mergeMap(async payload => {
+      const curr = await workloadService.checkStatus(payload);
+      if (curr.status === "WORKING") {
+        const work = await workloadService.cancel(payload);
+        return workloadsActions.updateStatus(work);
+      }
+      return workloadsActions.updateStatus(curr);
+    })
+  )
 )
+
+// const cancelWorkload: AppEpic = (action$, state$) => (
+//   action$.pipe(
+//     filter(isActionOf(workloadsActions.cancel)),
+//     map(action => action.payload),
+//     mergeMap(payload => merge(workloadService.cancel(payload)).pipe(
+//       map(payload => workloadsActions.updateStatus(payload)),
+//       // catchError(err => Promise.resolve({ type: 'Error', message: err.message })
+//     )))
+// )
+
+const watchWorkloads: AppEpic = (action$, state$) => (
+  action$.pipe(
+    filter(isActionOf(workloadsActions.created)),
+    map(action => action.payload),
+    mergeMap(payload =>
+      from(
+        // run on next tick
+        new Promise((res) => setTimeout(() => res(true), 0))
+      )
+        .pipe(
+          delay(payload.completeDate),
+          mergeMap(async () => {
+            const workLoad = await workloadService.checkStatus(payload);
+            console.log('workload after delay', workLoad)
+            return workloadsActions.updateStatus(workLoad);
+          })
+        )
+    )
+  ))
+
+// const watchWorkloads: AppEpic = (action$, state$) => (
+//   action$.pipe(
+//     filter(isActionOf(workloadsActions.created)),
+//     map(action => action.payload),
+//     mergeMap(payload =>
+//       of(payload).pipe(
+//         delay(payload.completeDate),
+//         mergeMap(async () => {
+//           const workLoad = await workloadService.checkStatus(payload);
+//           console.log('workload after delay', workLoad)
+//           return workloadsActions.updateStatus(workLoad);
+//         })
+//       )
+//     )
+//   ))
+
+// mergeMap(payload =>
+// merge(workloadService.checkStatus(payload))
+//   .pipe(map(payload => workloadsActions.updateStatus(payload)))
+//   .pipe(delay(payload.completeDate)))
 
 export const epics = combineEpics(
   logWorkloadSubmissions,
-  cancelWorkload
+  cancelWorkload,
+  watchWorkloads
 );
 
 export default epics;
